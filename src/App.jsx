500ddee62743a10076f231ddf9733ac23dc8b178
Eder Henrique <ederhscc@gmail.com>
2024-01-24 17:41:00 -0300

    updated TaxReport component for to show the results

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
 import TaxForm from "./components/TaxForm";
 import TaxReport from "./components/TaxReport";
 
-import { Container, Typography} from "@mui/material"
+import { Container, Typography } from "@mui/material";
 
-import {useState} from "react"
+import { useState } from "react";
 
 function App() {
-  const [textData, setTextData] = useState(null)
+  const [taxtData, setTaxtData] = useState(null);
 
   const calculateTax = (values) => {
-    console.log(values)
-  }
+    let tax = 0;
+
+    const income = parseFloat(values.income);
+
+    if (income <= 10000) {
+      tax = income * 0.05;
+    } else if (income <= 20000) {
+      tax = income * 0.1;
+    } else {
+      tax = income * 0.15;
+    }
+
+    const taxData = {
+      ...values,
+      tax,
+    };
+
+    setTaxtData(taxData)
+  };
 
   return (
-    <Container style={{maxWidth: "800px", margin: "0 auto"}}>
-      <Typography variant="h3" align="center" style={{marginBottom: "16px"}}>
+    <Container style={{ maxWidth: "800px", margin: "0 auto" }}>
+      <Typography variant="h3" align="center" style={{ marginBottom: "16px" }}>
         Calculadora de Impostos
       </Typography>
       <TaxForm onSubmit={calculateTax} />
-      {textData && <TaxReport />}
+      {taxtData && <TaxReport taxData={taxtData}/>}
     </Container>
   );
 }
