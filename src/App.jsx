d5c2955d12d35dd2aaf4b3bdcf2b02a42f5beba4
Eder Henrique <ederhscc@gmail.com>
2024-01-24 16:43:14 -0300

    structuring the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import TaxForm from "./components/TaxForm";
 import TaxReport from "./components/TaxReport";
 
+import { Container, Typography} from "@mui/material"
+
+import {useState} from "react"
+
 function App() {
+  const [textData, setTextData] = useState(null)
+
   return (
-    <>
+    <Container style={{maxWidth: "800px", margin: "0 auto"}}>
+      <Typography variant="h3" align="center" style={{marginBottom: "16px"}}>
+        Calculadora de Impostos
+      </Typography>
       <TaxForm />
-      <TaxReport />
-    </>
+      {textData && <TaxReport />}
+    </Container>
   );
 }
 
--- src/components/TaxReport.jsx
+++ src/components/TaxReport.jsx
@@ -1,6 +1,14 @@
+import {Container, Typography} from "@mui/material"
+
 const TaxReport = () => {
   return (
-    <div>TaxReport</div>
+    <Container>
+        <Typography variant="h5">Relatório de impostos</Typography>
+        <Typography variant="body1">Nome: </Typography>
+        <Typography variant="body1">Idade: anos</Typography>
+        <Typography variant="body1">Renda: </Typography>
+        <Typography variant="body1">Imposto a pagar: </Typography>
+    </Container>
   )
 }
 
